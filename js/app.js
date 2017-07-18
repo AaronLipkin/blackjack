1808d390a2ea873d89367ad3daa3b21a9212cb20
Aaron Lipkin <aj17@me.com>
2017-07-18 12:11:52 -0500

    Created deck of cards

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-$
+$(() => {
+	const cards = []
+	const suits = ['Diamonds','Hearts','Spades','Clubs'];
+	const faces = ['A',2,3,4,5,6,7,8,9,10,'Jack','Queen','King'];
+
+	//Creating a class for cards with their Blackjack value
+	class Card {
+		constructor(suit,face) {
+			this.suit = suit
+			this.face = face
+		}
+		evaluate() {
+		if (this.face === 'A'){
+			this.value = 11
+		}
+		else if (this.face === 'Jack' || this.face === 'Queen' || this.face === 'King') {
+			this.value = 10
+		}
+		else {
+			this.value = this.face;
+		}
+	}
+	}
+
+	//Creating a deck of cards
+	for (suit of suits) {
+		for (face of faces) {
+			newCard = new Card(suit,face)
+			newCard.evaluate()
+			cards.push(newCard)
+		}
+	}
+	console.log(cards)
+})
