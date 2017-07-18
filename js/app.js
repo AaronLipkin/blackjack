cf6d9f3e938359399f094779835ae3a81f763119
Aaron Lipkin <aj17@me.com>
2017-07-18 12:21:13 -0500

    Linked cards to their image

--- js/app.js
+++ js/app.js
@@ -10,16 +10,19 @@ $(() => {
 			this.face = face
 		}
 		evaluate() {
-		if (this.face === 'A'){
-			this.value = 11
+			if (this.face === 'A'){
+				this.value = 11
+				this.image = 'images/cards/ace_of_' + this.suit.toLowerCase() + '.png'
+			}
+			else if (this.face === 'Jack' || this.face === 'Queen' || this.face === 'King') {
+				this.value = 10
+				this.image = "images/cards/" + this.face + "_of_" + this.suit.toLowerCase() + '.png'
+			}
+			else {
+				this.value = this.face;
+				this.image = "images/cards/" + this.face + "_of_" + this.suit.toLowerCase() + '.png'
+			}
 		}
-		else if (this.face === 'Jack' || this.face === 'Queen' || this.face === 'King') {
-			this.value = 10
-		}
-		else {
-			this.value = this.face;
-		}
-	}
 	}
 
 	//Creating a deck of cards
